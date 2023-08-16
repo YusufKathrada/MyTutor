b9aa015c0ab843391ad7339a18c9f0c583feffc5
Yusuf Kathrada <kthyus001@myuct.ac.za>
2023-08-16 18:18:35 +0200

    Added the select time slot feature for demo

--- src/app/pages/select-times/select-times.page.ts
+++ src/app/pages/select-times/select-times.page.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectTimesPage implements OnInit {
 
+  sessions: any = [{
+      tutorialNumber: "Tutorial 1",
+      day: "Mond",
+      time: "08:00 - 10:00",
+      availability: "3/4",
+      action: "join",
+      full: false 
+    },
+    {
+      tutorialNumber: "Tutorial 2",
+      day: "Tues",
+      time: "09:00 - 11:00",
+      availability: "5/5",
+      action: "full",
+      full: true
+    }  
+  ];
+
   constructor() { }
 
   ngOnInit() {
