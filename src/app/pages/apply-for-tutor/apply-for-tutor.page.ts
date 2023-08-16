dfb0b778f727052f10536020821477c650a9d0f6
Dayaan Jappie <jppday001@myuct.ac.za>
2023-08-16 22:42:05 +0200

    apply for tutor function

--- src/app/pages/apply-for-tutor/apply-for-tutor.page.ts
+++ src/app/pages/apply-for-tutor/apply-for-tutor.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-apply-for-tutor',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./apply-for-tutor.page.scss'],
 })
 export class ApplyForTutorPage implements OnInit {
+  tutor = {
+    name: '',
+    surname: '',
+    studentNumber: '',
+    degreeOfStudy: '',
+    yearOfStudy: '',
+    desiredCourse: '',
+    previousMark: '',
+    transcript: null
+  };
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      console.log('Tutor info:', this.tutor);
+      // Send the form data to your server or handle it as needed
+    }
+  }
+
+  onFileChange(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.tutor.transcript = file;
+    }
+  }
 }
+
