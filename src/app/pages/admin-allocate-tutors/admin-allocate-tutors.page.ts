aa84b871a66f2ab856d678e4eca6d6a5c2b4bc9f
Yusuf Kathrada <kthyus001@myuct.ac.za>
2023-08-22 15:24:36 +0200

    Added features to Admin Allocate Tutors Page

--- src/app/pages/admin-allocate-tutors/admin-allocate-tutors.page.ts
+++ src/app/pages/admin-allocate-tutors/admin-allocate-tutors.page.ts
@@ -7,9 +7,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminAllocateTutorsPage implements OnInit {
 
+  assignedTutors: any = [{
+    studentName: "John Doe",
+    studentNumber: "DOEJHN001",
+    assignedCourse: "",
+  },
+  {
+    studentName: "Mary Adams",
+    studentNumber: "MRYADM001",
+    assignedCourse: "",
+  }  
+];
+
+  //Basic courses added for now, proper list should be updated in future
+  Course: any = [
+  {
+    courseName: "",
+    courseCode: "Unassigned"
+  },
+  {
+    courseName: "Intro to Computer Science",
+    courseCode: "CSC1001F"
+  },
+  {
+  courseName: "Intro to Java",
+    courseCode: "CSC1002S"
+  },
+  {
+    courseName: "Data Structures and Algorithms",
+      courseCode: "CSC2001F"
+  },
+  {
+    courseName: "Parallel Computing",
+      courseCode: "CSC2002S"
+  },
+  {
+    courseName: "Networks and Operating Systems",
+      courseCode: "CSC3001F"
+  },
+  {
+    courseName: "Advanced Software Development",
+      courseCode: "CSC3002S"
+  },
+]
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  test(){
+    console.log("Tutors", this.assignedTutors)
   }
 
 }
