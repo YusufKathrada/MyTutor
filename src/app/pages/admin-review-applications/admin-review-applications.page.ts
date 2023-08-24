204c011bbf9930353ec1b070d7d842a1833092d5
Yusuf Kathrada <kthyus001@myuct.ac.za>
2023-08-24 15:28:46 +0200

    Populated table for Admin Reviews Application page

--- src/app/pages/admin-review-applications/admin-review-applications.page.ts
+++ src/app/pages/admin-review-applications/admin-review-applications.page.ts
@@ -7,9 +7,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminReviewApplicationsPage implements OnInit {
 
+  tutorApplications: any = [
+    {
+      studentName: "John Doe",
+      degree: "BSc CS",
+      yearOfStudy: "2",
+      average: 76,
+      desiredCourse: "CSC1001F",
+      status: ""
+    },
+    {
+      studentName: "Steven Adams",
+      degree: "BBusSc CS",
+      yearOfStudy: "3",
+      average: 72,
+      desiredCourse: "CSC2002S",
+      status: ""
+    },
+    {
+      studentName: "Mary Lamb",
+      degree: "BSc CS",
+      yearOfStudy: "3",
+      average: 64,
+      desiredCourse: "CSC2001F",
+      status: ""
+    }
+  ]
+
+  applicationStatus: any = [
+    {
+      status: "Accepted"
+    },
+    {
+      status: "Pending"
+    },
+    {
+      status: "Rejected"
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  test(){
+    console.log(this.tutorApplications)
   }
 
 }
