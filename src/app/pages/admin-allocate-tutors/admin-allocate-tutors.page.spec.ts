67588ade6a6115430a93c8ac5635c22d23c8131f
jaedonvs <jaedonvs@gmail.com>
2023-09-25 13:31:10 +0200

    unit testing

--- src/app/pages/admin-allocate-tutors/admin-allocate-tutors.page.spec.ts
+++ src/app/pages/admin-allocate-tutors/admin-allocate-tutors.page.spec.ts
@@ -1,7 +1,16 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
-
 import { AdminAllocateTutorsPage } from './admin-allocate-tutors.page';
+import { MockStorage } from '../../../services/mock-storage.service';
+import { Admin } from '../../providers/admin';
+
+TestBed.configureTestingModule({
+  declarations: [
+    AdminAllocateTutorsPage,
+  ],
+  schemas: [NO_ERRORS_SCHEMA]
+}).compileComponents();
 
 describe('AdminAllocateTutorsPage', () => {
   let component: AdminAllocateTutorsPage;
@@ -10,7 +19,12 @@ describe('AdminAllocateTutorsPage', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AdminAllocateTutorsPage ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot()],
+      providers: [
+        Admin,
+        { provide: Storage, useClass: MockStorage },
+        { provide: Admin, useValue: { storage: new MockStorage() } },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AdminAllocateTutorsPage);
@@ -20,5 +34,39 @@ describe('AdminAllocateTutorsPage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should change segment value', () => {
+    component.segment = 'tutor';
+    fixture.detectChanges();
+    expect(component.segment).toEqual('tutor');
+  });
+
+  it('should call applyTutorFilter when the segment is tutor and filter is changed', () => {
+    spyOn(component, 'applyTutorFilter');
+    component.segment = 'tutor';
+    component.filterOption = 'assigned';
+    component.applyTutorFilter(); // Call this method manually as it’s normally called by (ionChange) event.
+    expect(component.applyTutorFilter).toHaveBeenCalled();
+  });
+
+  it('should call applyTAFilter when the segment is TA and filter is changed', () => {
+    spyOn(component, 'applyTAFilter');
+    component.segment = 'TA';
+    component.filterOption = 'assigned';
+    component.applyTAFilter(); // Call this method manually as it’s normally called by (ionChange) event.
+    expect(component.applyTAFilter).toHaveBeenCalled();
+  });
+
+  it('should call updateTutorAllocations method when the update button is clicked for tutor segment', () => {
+    spyOn(component, 'updateTutorAllocations');
+    component.updateTutorAllocations(); // Call this method manually as it’s normally called by (click) event.
+    expect(component.updateTutorAllocations).toHaveBeenCalled();
+  });
+
+  it('should call updateTAAllocations method when the update button is clicked for TA segment', () => {
+    spyOn(component, 'updateTAAllocations');
+    component.updateTAAllocations(); // Call this method manually as it’s normally called by (click) event.
+    expect(component.updateTAAllocations).toHaveBeenCalled();
   });
 });
--- src/app/pages/apply-for-tutor/apply-for-tutor.page.spec.ts
+++ src/app/pages/apply-for-tutor/apply-for-tutor.page.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { Storage } from '@ionic/storage-angular'; // Import Storage
+import { MockStorage } from '../../../services/mock-storage.service';
+import { By } from '@angular/platform-browser';
+import { Student } from '../../providers/student';
+import { TA } from '../../providers/ta';
+import { NgForm } from '@angular/forms';
 import { ApplyForTutorPage } from './apply-for-tutor.page';
 
 describe('ApplyForTutorPage', () => {
@@ -9,8 +15,16 @@ describe('ApplyForTutorPage', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ ApplyForTutorPage ],
-      imports: [IonicModule.forRoot()]
+      declarations: [ApplyForTutorPage],
+      imports: [IonicModule.forRoot()],
+      providers: [
+        Student,
+        TA,
+        { provide: Storage, useClass: MockStorage }, // Corrected Storage provider
+        { provide: Student, useValue: { storage: new MockStorage() } },
+        { provide: TA, useValue: { storage: new MockStorage() } },
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ApplyForTutorPage);
@@ -20,5 +34,26 @@ describe('ApplyForTutorPage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call onSubmit when the form is submitted', () => {
+    const spy = spyOn(component, 'onSubmit');
+    const form = new NgForm([], []);
+    component.onSubmit(form);
+    expect(spy).toHaveBeenCalledWith(form);
+  });
+
+  it('should call onFileChangeTutor when file input changes in Tutor form', () => {
+    const spy = spyOn(component, 'onFileChangeTutor');
+    const event = { target: { files: ['file1'] } } as any;
+    component.onFileChangeTutor(event);
+    expect(spy).toHaveBeenCalledWith(event);
+  });
+
+  it('should call onFileChangeTA when file input changes in TA form', () => {
+    const spy = spyOn(component, 'onFileChangeTA');
+    const event = { target: { files: ['file1'] } } as any;
+    component.onFileChangeTA(event);
+    expect(spy).toHaveBeenCalledWith(event);
   });
 });
